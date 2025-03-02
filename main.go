c76ee8511a4283f5095d455a3c5d18a53f20dfda
k3vwdd <kevwdiaz@outlook.com>
2025-03-02 14:57:26 -0600

    feature: middlewareLogging setup along with helper functions taken from "https://blog.boot.dev/golang/json-golang/"

--- main.go
+++ main.go
@@ -1,9 +1,13 @@
 package main
 
 import (
+	"bytes"
+	"encoding/json"
 	"fmt"
+	"io"
 	"log"
 	"net/http"
+	"unicode/utf8"
 
 	"go.uber.org/atomic"
 )
@@ -11,11 +15,25 @@ import (
 type apiConfig struct {
     fileServerHits atomic.Int32
 }
+func middlewareLogging(next http.Handler) http.Handler {
+    return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+        bodyBytes, err := io.ReadAll(r.Body)
+        if err != nil {
+            http.Error(w, "Failed to read request body", http.StatusInternalServerError)
+            return
+        }
+        log.Printf("Request body: %s\n", string(bodyBytes))
+        // Put it back so the handler can read it again
+        r.Body = io.NopCloser(bytes.NewBuffer(bodyBytes))
+        next.ServeHTTP(w, r)
+    })
+}
+
 
 func (cfg *apiConfig) middlewareMetricsInc(next http.Handler) http.Handler {
     return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-        next.ServeHTTP(w, r)
         cfg.fileServerHits.Add(1)
+        next.ServeHTTP(w, r)
     })
 }
 
@@ -24,11 +42,11 @@ func (cfg *apiConfig) middlewareMetricsInc(next http.Handler) http.Handler {
 
 func (cfg *apiConfig) handleWriteHits(w http.ResponseWriter, r *http.Request) {
     html := `
-     <html>
-      <body>
-        <h1>Welcome, Chirpy Admin</h1>
-        <p>Chirpy has been visited %v times!</p>
-      </body>
+    <html>
+    <body>
+    <h1>Welcome, Chirpy Admin</h1>
+    <p>Chirpy has been visited %v times!</p>
+    </body>
     </html>
     `
     if r.Method != http.MethodGet {
@@ -38,6 +56,62 @@ func (cfg *apiConfig) handleWriteHits(w http.ResponseWriter, r *http.Request) {
     w.Header().Set("Content-Type" , "text/html; charset=utf-8")
     w.WriteHeader(http.StatusOK)
     fmt.Fprintf(w, html, cfg.fileServerHits.Load())
+}
+
+func respondWithJSONHelper(w http.ResponseWriter, statuscode int, payload interface{}) error {
+    response, err := json.Marshal(payload)
+    if err != nil {
+        return err
+    }
+    w.Header().Set("Content-Type", "application/json")
+    w.Header().Set("Access-Control-Allow-Origin", "*")
+    w.WriteHeader(statuscode)
+    w.Write(response)
+    return nil
+}
+
+func respondWithErrorHelper(w http.ResponseWriter, statuscode int, msg string) error {
+    return respondWithJSONHelper(w, statuscode, map[string]string{"error": msg})
+}
+
+func handleChirpChars(w http.ResponseWriter, r *http.Request) {
+    if r.Method != http.MethodPost {
+        http.Error(w, "Method Not Allowed", http.StatusNotFound)
+        return
+    }
+
+    defer r.Body.Close()
+
+    type requestBody struct {
+        Body string `json:"body"`
+    }
+
+    type responseBody struct {
+        Valid string `json:"valid"`
+    }
+
+    data, err := io.ReadAll(r.Body)
+    if err != nil {
+        respondWithErrorHelper(w, 500, "couldn't read request")
+        return
+    }
+
+    params := requestBody{}
+    err = json.Unmarshal(data, &params)
+    if err != nil {
+        respondWithErrorHelper(w, 500, "couldn't unmarshal parameters")
+        return
+    }
+
+    characterCount := utf8.RuneCountInString(params.Body)
+    if characterCount > 140 {
+        respondWithErrorHelper(w, 400, "Chirp is too long")
+        return
+    }
+
+    respondWithJSONHelper(w, 200, responseBody{
+        Valid: "true",
+    })
 }
 
 func (cfg *apiConfig) handleRegister(w http.ResponseWriter, r *http.Request) {
@@ -62,16 +136,17 @@ func handleHealthReadiness(w http.ResponseWriter, r *http.Request) {
 }
 
 func main() {
-
     cfg := &apiConfig{}
 
     filepathRoot := "."
     mux := http.NewServeMux()
+    loggedMux := middlewareLogging(mux)
     mux.Handle("/", http.FileServer(http.Dir(filepathRoot)))
     // strips "/" off of /app/
 
     mux.Handle("/app/", http.StripPrefix("/app/", cfg.middlewareMetricsInc((http.FileServer(http.Dir(filepathRoot))))))
     mux.HandleFunc("/api/healthz", handleHealthReadiness)
+    mux.HandleFunc("/api/validate_chirp", handleChirpChars)
     mux.HandleFunc("/admin/metrics", cfg.handleWriteHits)
     mux.HandleFunc("/admin/reset", cfg.handleRegister)
 
@@ -79,7 +154,7 @@ func main() {
     // a struct that describes a server configuration
     server := &http.Server{
         Addr: ":" + port,
-        Handler: mux,
+        Handler: loggedMux,
     }
 
     err := server.ListenAndServe()
